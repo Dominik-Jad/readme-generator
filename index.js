f844a289bd5094ee37bae81d68fea667115a5269
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-02-16 18:26:10 +0000

    added questions to the array and created a template to writeToFile

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-    
+    "What is your GitHub username?",
+    "What is your email?",
+    "What is the title of your project?",
+    "What is the description of your project?",
+    "What are the installation instructions?",
+    "What is the usage information?",
+    "What are the contribution guidelines?",
+    "What are the test instructions?",
+    "What license would you like to use?"
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Read me Generated!')
+    );
 }
 
 // function to initialize program
@@ -19,3 +30,32 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name?',
+//     },
+//     {
+//       type: 'checkbox',
+//       message: 'What languages do you know?',
+//       name: 'stack',
+//       choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+//     },
+//     {
+//       type: 'list',
+//       message: 'What is your preferred method of communication?',
+//       name: 'contact',
+//       choices: ['email', 'phone', 'telekinesis'],
+//     },
+//   ])
+//   .then((data) => {
+//     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+//       err ? console.log(err) : console.log('Success!')
+//     );
+//   });
