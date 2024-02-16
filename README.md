fe6dbc060174e8a582a1e04218388fb6f1439e76
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-02-16 18:43:40 +0000

    added string literals to the generateMarkdown js file

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Read me Generator
+# readme gen   ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+ 
+  ## Description
+  read me 
+  ## Table of Contents
 
+  <details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li><a href="#Description">Description</a></li>
+    <li><a href="#Installation">Installation</a></li>
+    <li><a href="#Usage">Usage</a></li>
+    <li><a href="#Contributing">Contributing</a></li>
+    <li><a href="#Tests">Tests</a></li>
+    <li><a href="#Questions">Questions</a></li>
+    <li><a href="#License">License</a></li>
+  </ol>
+  </details>
+
+  ## Installation
+  npm i 
+
+  ## Usage
+  read me 
+
+  ## Contributing
+  undefined
+
+  ## Tests
+  undefined
+
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at dominik. You can find more of my work at [Read me gen]
+  
+  ## License
+  This project is licensed under the MIT license
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}  ![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+ 
+  ## Description
+  ${data.description}
+  ## Table of Contents
 
+  <details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li><a href="#Description">Description</a></li>
+    <li><a href="#Installation">Installation</a></li>
+    <li><a href="#Usage">Usage</a></li>
+    <li><a href="#Contributing">Contributing</a></li>
+    <li><a href="#Tests">Tests</a></li>
+    <li><a href="#Questions">Questions</a></li>
+    <li><a href="#License">License</a></li>
+  </ol>
+  </details>
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.username}]
+  
+  ## License
+  This project is licensed under the ${data.license} license
 `;
 }
 
